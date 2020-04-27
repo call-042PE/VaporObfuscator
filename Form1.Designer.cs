77e6b28ab36a5a57d3244fd5e74ed0094c2d0c66
Mr. Anderson <44845040+call-042PE@users.noreply.github.com>
2020-04-27 08:43:42 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace VaporObfuscator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(421, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 22);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ｇｏ！　（日意ボ）";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(164, 49);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox1.Size = new System.Drawing.Size(240, 15);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ｆｉｌｅ　Ｐａｔｈ　炎ちー";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(12, 108);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(213, 19);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "【﻿Ｎａｍｅ　Ｐｒｏｔｅｃｔｉｏｎ】";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Location = new System.Drawing.Point(228, 367);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "(っ◔◡◔)っ ♥ Protect ! ♥";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.Location = new System.Drawing.Point(12, 131);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(334, 19);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "【﻿Ｆａｋｅ　Ｏｂｆｕｓｃａｔｉｏｎ　Ａｔｔｒｉｂｕｔｅ】";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox3.Location = new System.Drawing.Point(12, 154);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(202, 19);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "【﻿Ｊｕｎｋ　Ａｔｔｒｉｂｕｔｅ】";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Purple;
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(-5, -8);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(640, 48);
+            this.panel2.TabIndex = 9;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            this.panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel2_mousedown);
+            this.panel2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel2_mousemove);
+            this.panel2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel2_mouseup);
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button3.Location = new System.Drawing.Point(605, 10);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(22, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "X";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(200, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(195, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "【﻿ＶａｐｏｒＯｂｆｕｓｃａｔｏｒ】";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Purple;
+            this.panel3.Location = new System.Drawing.Point(-5, 396);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(664, 62);
+            this.panel3.TabIndex = 9;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.AllowDrop = true;
+            this.trackBar1.LargeChange = 10;
+            this.trackBar1.Location = new System.Drawing.Point(450, 182);
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(172, 45);
+            this.trackBar1.SmallChange = 10;
+            this.trackBar1.TabIndex = 10;
+            this.trackBar1.Value = 10;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(470, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 15);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Ｏｐａｃｉｔｙ　ソどゕ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(184, 310);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(206, 15);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "ｂｙ ｋａｍｉ． ｔｈｅ ｆｉｊｉ ｇｕｙ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(369, 154);
+            this.textBox2.MaxLength = 99;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(49, 13);
+            this.textBox2.TabIndex = 13;
+            this.textBox2.Text = "100";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(216, 154);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(10, 15);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "|";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(229, 152);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(134, 17);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "ɴᴜᴍʙᴇʀ ᴏꜰ ᴀᴛᴛʀɪʙᴜᴛᴇ:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(3, 260);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(178, 15);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "【﻿Ｆｉｌｅ　Ｎａｍｅ　ＵＷＵ】: ";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Location = new System.Drawing.Point(187, 260);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 13);
+            this.textBox3.TabIndex = 17;
+            this.textBox3.Text = "Confused";
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox6.Location = new System.Drawing.Point(11, 179);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(213, 19);
+            this.checkBox6.TabIndex = 19;
+            this.checkBox6.Text = "【﻿Ｅｎｃｒｙｐｔ　Ｓｔｒｉｎｇｓ】";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            this.checkBox6.CheckedChanged += new System.EventHandler(this.checkBox6_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Orchid;
+            this.ClientSize = new System.Drawing.Size(634, 454);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.CheckBox checkBox6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+
+namespace VaporObfuscator
+{
+    public partial class Form1 : Form
+    {
+        string[] attrib = { "YanoAttribute", "Xenocode.Client.Attributes.AssemblyAttributes.ProcessedByXenocode", "PoweredByAttribute", "ObfuscatedByGoliath", "NineRays.Obfuscator.Evaluation", "NetGuard", "dotNetProtector", "DotNetPatcherPackerAttribute", "DotNetPatcherObfuscatorAttribute", "DotfuscatorAttribute", "CryptoObfuscator.ProtectedWithCryptoObfuscatorAttribute", "BabelObfuscatorAttribute", "BabelAttribute", "AssemblyInfoAttribute" };
+        int mov;
+        int movX;
+        int movY;
+        bool nameprotection = false;
+        bool fakeobfu = false;
+        bool junkattribute = false;
+        bool antitamper = false;
+        bool encryptstring = false;
+        bool antidebug = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private static Random random = new Random();
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        public void ProtectName(AssemblyDef assembly, ModuleDef mod)
+        {//rename module
+            //rename type
+            foreach (TypeDef type in mod.Types)
+            {
+                mod.Name = "ObfuscatedByVapor";
+                type.Name = RandomString(20) + "俺ム仮 ｎｏ ｓｌｅｅｐ俺ム仮";
+                type.Namespace = RandomString(20) + "俺ム仮 ｎｏ ｓｌｅｅｐ俺ム仮";
+                foreach (PropertyDef property in type.Properties)
+                {
+                    property.Name = RandomString(20) + "俺ム仮 ｎｏ ｓｌｅｅｐ俺ム仮";
+                }
+                foreach (FieldDef fields in type.Fields)
+                {
+                    fields.Name = RandomString(20) + "俺ム仮 ｎｏ ｓｌｅｅｐ俺ム仮";
+                }
+                foreach(EventDef eventdef in type.Events)
+                {
+                    eventdef.Name = RandomString(20) + "俺ム仮 ｎｏ ｓｌｅｅｐ俺ム仮";
+                }
+                foreach(MethodDef method in type.Methods)
+                {
+                    if(!method.IsConstructor)
+                        method.Name = RandomString(20) + "俺ム仮 ｎｏ ｓｌｅｅｐ俺ム仮";
+                }
+            }
+        }
+
+        public void fakeobfuscation(ModuleDefMD module)
+        {
+            for (int i = 0; i < attrib.Length; i++)
+            {
+                var fakeattrib = new TypeDefUser(attrib[i],attrib[i],module.CorLibTypes.Object.TypeDefOrRef);
+                fakeattrib.Attributes = TypeAttributes.Class | TypeAttributes.NotPublic | TypeAttributes.WindowsRuntime;
+                module.Types.Add(fakeattrib);
+
+            }
+        }
+
+        public void junkattrib(ModuleDefMD module)
+        {
+            int number = System.Convert.ToInt32(textBox2.Text);
+            for(int i = 0; i < number; i++)
+            {
+                var junkattribute = new TypeDefUser("俺ム仮 ｎｏ　ｓｌｅｅｐ　俺ム仮"+RandomString(20), "俺ム仮 ｎｏ　ｓｌｅｅｐ　俺ム仮"+RandomString(20), module.CorLibTypes.Object.TypeDefOrRef);
+                module.Types.Add(junkattribute);
+            }
+        }
+
+        public void encryptString(ModuleDefMD module)
+        {
+            foreach (TypeDef type in module.Types)
+            {
+                foreach(MethodDef method in type.Methods)
+                {
+                if (method.Body == null) continue;
+                    for(int i = 0; i < method.Body.Instructions.Count(); i++)
+                    {
+                        if(method.Body.Instructions[i].OpCode == OpCodes.Ldstr)
+                        {
+                            string oldstring = method.Body.Instructions[i].Operand.ToString();
+                            string newstring = Convert.ToBase64String(UTF8Encoding.UTF8.GetBytes(oldstring));
+                            method.Body.Instructions[i].OpCode = OpCodes.Nop;
+                            method.Body.Instructions.Insert(i + 1, new Instruction(OpCodes.Call, module.Import(typeof(System.Text.Encoding).GetMethod("get_UTF8", new Type[] { }))));
+                            method.Body.Instructions.Insert(i + 2, new Instruction(OpCodes.Ldstr, newstring));
+                            method.Body.Instructions.Insert(i + 3, new Instruction(OpCodes.Call, module.Import(typeof(System.Convert).GetMethod("FromBase64String", new Type[] { typeof(string) }))));
+                            method.Body.Instructions.Insert(i + 4, new Instruction(OpCodes.Callvirt, module.Import(typeof(System.Text.Encoding).GetMethod("GetString", new Type[] { typeof(byte[]) }))));
+                            i += 4;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openfiledialog = new OpenFileDialog();
+            openfiledialog.Filter = "Executables | *.*";
+            openfiledialog.ShowDialog();
+            textBox1.Text = openfiledialog.FileName;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            nameprotection = !nameprotection;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        { 
+            try
+            {
+                ModuleDefMD module = ModuleDefMD.Load(textBox1.Text);
+                AssemblyDef assembly = AssemblyDef.Load(textBox1.Text);
+                if(nameprotection == true)
+                {
+                    ProtectName(assembly, module);
+                    module.Write(textBox3.Text + ".exe");
+                }
+                if (fakeobfu == true)
+                {
+                    fakeobfuscation(module);
+                    module.Write(textBox3.Text + ".exe");
+                }
+                if(junkattribute == true)
+                {
+                    junkattrib(module);
+                    module.Write(textBox3.Text + ".exe");
+                }
+                if(encryptstring == true)
+                {
+                    encryptString(module);
+                    module.Write(textBox3.Text + ".exe");
+                }
+                MessageBox.Show("🍓  🎀  𝕌𝕎𝕌 𝕋𝕙𝕒𝕟𝕜𝕤 𝕗𝕠𝕣 𝕦𝕤𝕚𝕟𝕘 𝕍𝕒𝕡𝕠𝕣𝕆𝕓𝕗𝕦𝕤𝕔𝕒𝕥𝕠𝕣 𝕌𝕎𝕌  🎀  🍓", "🍓  🎀  𝕌𝕎𝕌 𝕋𝕙𝕒𝕟𝕜𝕤 𝕗𝕠𝕣 𝕦𝕤𝕚𝕟𝕘 𝕍𝕒𝕡𝕠𝕣𝕆𝕓𝕗𝕦𝕤𝕔𝕒𝕥𝕠𝕣 𝕌𝕎𝕌  🎀  🍓", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        catch (Exception)
+            {
+                MessageBox.Show("An error has occured !", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            fakeobfu = !fakeobfu;
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            junkattribute = !junkattribute;
+        }
+
+        private void checkBox6_CheckedChanged(object sender, EventArgs e)
+        {
+            encryptstring = !encryptstring;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Location = Screen.AllScreens[0].WorkingArea.Location;
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            antitamper = !antitamper;
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel2_mousedown(object sender, MouseEventArgs e)
+        {
+            mov = 1;
+            movX = e.X;
+            movY = e.Y;
+        }
+
+        private void panel2_mousemove(object sender, MouseEventArgs e)
+        {
+            if(mov == 1)
+            {
+                this.SetDesktopLocation(MousePosition.X - movX, MousePosition.Y - movY);
+            }
+        }
+
+        private void panel2_mouseup(object sender, MouseEventArgs e)
+        {
+            mov = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            this.Opacity = ((double)trackBar1.Value /trackBar1.Maximum);
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+            antidebug = !antidebug;
+        }
+    }
+}
